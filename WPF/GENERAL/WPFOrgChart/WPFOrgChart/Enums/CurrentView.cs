4c1d2ad74132d1ed37e25c1f36bfcf8e7642e5f8
fred <domaine75@gmail.com>
2023-07-21 14:14:18 +0200

    clean code

--- WPF/GENERAL/WPFOrgChart/WPFOrgChart/Enums/CurrentView.cs
+++ WPF/GENERAL/WPFOrgChart/WPFOrgChart/Enums/CurrentView.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WPFOrgChart.Enums {
-    public enum CurrentView {
-       DataEntry,
-       OrgChart
-    }
+﻿namespace WPFOrgChart.Enums
+{
+  public enum CurrentView
+  {
+    DataEntry,
+    OrgChart
+  }
 }
